310330fae7b5dc681bd1c3c6e3a64c2d174d1d34
JHYOOOOON <jhyooooooon@gmail.com>
2022-04-04 20:50:54 +0900

    🌊 상근이의 여행

---
+++ boj/상근이의여행.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+
+    while (T--)
+    {
+        int N, M, a, b;
+        cin >> N >> M;
+        while (M--)
+        {
+            cin >> a >> b;
+        }
+        cout << N - 1 << "\n";
+    }
+
+    return 0;
+}
