03806e12f6093860a669ab6020145d9669b02bb6
jhyoon <jhyooooooon@gmail.com>
2023-01-20 13:03:05 +0900

    🌟 TwoSum

--- leetcode/TwoSum.js
+++ leetcode/TwoSum.js
@@ -4,9 +4,20 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-	for (let i = 0; i < nums.length; i++) {
-		for (let j = i + 1; j < nums.length; j++) {
-			if (nums[i] + nums[j] === target) return [i, j];
+	const sortNums = nums.map((num, index) => [num, index]);
+	sortNums.sort((a, b) => a[0] - b[0]);
+	let left = 0;
+	let right = nums.length - 1;
+	while (left < right) {
+		const [num_1, index_1] = sortNums[left];
+		const [num_2, index_2] = sortNums[right];
+		if (num_1 + num_2 === target) {
+			return [index_1, index_2];
+		}
+		if (num_1 + num_2 < target) {
+			left++;
+		} else {
+			right--;
 		}
 	}
 };
