802e65d72ed8ca35587979324ded59be0571cc76
jhyoon <jhyooooooon@gmail.com>
2023-05-22 18:10:45 +0900

    🌱 홀짝 구분하기

---
+++ programmers/홀짝구분하기.js
@@ -0,0 +1,14 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+	input = line.split(" ");
+}).on("close", function () {
+	n = Number(input[0]);
+	console.log(`${n} is ${n % 2 ? "odd" : "even"}`);
+});
