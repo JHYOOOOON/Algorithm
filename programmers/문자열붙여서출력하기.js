956ba1bf3ae9e8c94231748b30970a2e9b9682f6
jhyoon <jhyooooooon@gmail.com>
2023-05-23 21:42:16 +0900

    🌱 문자열 붙여서 출력하기

---
+++ programmers/문자열붙여서출력하기.js
@@ -0,0 +1,13 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = line.split(' ');
+}).on('close', function () {
+    console.log(`${input[0]}${input[1]}`);
+});
