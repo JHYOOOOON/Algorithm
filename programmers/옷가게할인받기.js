6f134ec5daa826bed0f98615a9e12cf1cf9a1ec1
JHYOOOOON <jhyooooooon@gmail.com>
2023-11-29 14:06:24 +0900

    fix: 정답 코드 업데이트

--- programmers/옷가게할인받기.js
+++ programmers/옷가게할인받기.js
@@ -1,9 +1,10 @@
 function solution(price) {
-	const discount = (price) => {
-		if (price >= 500000) return price * 0.8;
-		if (price >= 300000) return price * 0.9;
-		if (price >= 100000) return price * 0.95;
-		return price;
-	};
-	return discount(price);
+	if (price >= 500000) {
+		return Math.floor(price * 0.8);
+	} else if (price >= 300000) {
+		return Math.floor(price * 0.9);
+	} else if (price >= 100000) {
+		return Math.floor(price * 0.95);
+	}
+	return price;
 }
